7ff349feea57b0685a6d782e776a4149829aba6b
Aleksandr.Fedorov <aleksandr.fedorov@billing.ru>
2018-06-03 20:58:33 +0300

    refactoring

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ rm -rf /tmp/dashboard/*
 docker run --name=dashboard --privileged=true  dashboard
 docker cp front:/usr/src/app/dist /tmp/dashboard
 mkdir -p /var/www/dashboard
-cp -Rf /tmp/dashboard/* /var/www/dashboard/
+cp -Rf /tmp/dashboard/dist/* /var/www/dashboard/
