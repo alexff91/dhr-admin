be633858d4eb6b9a1f0c20dbcff6d0647f565703
lesha <oboshto@gmail.com>
2018-06-12 21:18:31 +0300

    vacancies table improvments

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,7 +4,7 @@ const BACKEND_URL = 'https://168.63.13.234:8082';
 
 const instance = axios.create({
   baseURL: `${BACKEND_URL}/api/v1`,
-  timeout: 5000
+  // timeout: 5000
 });
 
 export default instance;
--- src/views/pages/vacancies.vue
+++ src/views/pages/vacancies.vue
@@ -4,26 +4,47 @@
             <h1 class="title">Список вакансий</h1>
         </div>
 
-        <router-link :to="`/vacancies/${vacancy.id}`" v-for="vacancy in vacancies" :key="vacancy.id" class="item-row">
-            <div>
-                {{vacancy.position}}
+        <div class="imba-table">
+            <div class="imba-row imba-row-head">
+                <div class="imba-col imba-col-main">Позиция</div>
+                <div class="imba-col">Статус</div>
+                <div class="imba-col">Отклики</div>
+                <div class="imba-col imba-col-small">Создана</div>
             </div>
-            <div style="margin-left: auto;">
-                {{new Date(vacancy.creationDate).toLocaleString()}}
-            </div>
-        </router-link>
+            <router-link :to="`/vacancies/${vacancy.id}`" v-for="vacancy in vacancies" :key="vacancy.id" class="vacancy-row imba-row">
+                <div class="imba-col imba-col-main">
+                    {{vacancy.position}}
+                </div>
+                <div class="imba-col">
+                    В работе
+                </div>
+                <div class="imba-col">
+                    11/65
+                </div>
+                <div class="vacancy-creation-time imba-col imba-col-small">
+                    <vk-icon icon="clock" class="icon" :ratio="0.7"></vk-icon>
+                    <span>{{ distanceInWords(new Date(vacancy.creationDate), new Date(), { locale: ru }) }} назад</span>
+                </div>
+            </router-link>
+        </div>
     </div>
 </template>
 
 <script>
   import { Companies } from '../../api';
   import { mapGetters } from 'vuex';
+  import { distanceInWords } from 'date-fns';
+  import ru from 'date-fns/locale/ru';
+  import VkIcon from 'vuikit/src/library/icon/components/icon';
 
   export default {
     name: 'vacancies',
+    components: {VkIcon},
     data() {
       return {
-        vacancies: []
+        vacancies: [],
+        distanceInWords,
+        ru
       };
     },
     computed: mapGetters({
@@ -44,7 +65,7 @@
 
         Companies.getVacancies(this.company.id)
           .then(res => {
-            this.vacancies = res.data;
+            this.vacancies = res.data.sort((a, b) => b.creationDate - a.creationDate);
           });
       }
     }
@@ -54,18 +75,64 @@
 <style lang="scss">
     @import "../../assets/styles/variables";
 
-    .item-row {
+    .imba-row {
         display: flex;
         flex-direction: row;
+        align-items: center;
+        margin-bottom: .5rem;
+
+        padding: 0 1rem;
+
+        .imba-col {
+            flex-grow: 3;
+            flex-shrink: 0;
+            flex-basis: 75px;
+        }
+
+        .imba-col-small {
+            flex-grow: 1;
+        }
+
+        .imba-col-main {
+            flex-basis: 250px;
+            flex-grow: 0;
+        }
+    }
+
+    .vacancy-row {
         border-radius: 3px;
         background-color: #fff;
-        margin-bottom: .5rem;
         padding: 1rem;
         color: $base-color;
         text-decoration: none;
+        height: 60px;
 
         &:hover {
-            box-shadow: 0 0 0 2px $brand-color;
+            box-shadow: 0 0 0 2px $primary-color;
+        }
+    }
+
+    .imba-row-head {
+        background-color: transparent;
+        font-size: 14px;
+        font-weight: bold;
+        color: grey
+    }
+
+    .vacancy-creation-time {
+        display: flex;
+        align-items: center;
+        margin-left: auto;
+        color: $secondary-color;
+        font-size: 12px;
+
+        .icon {
+            margin-right: 4px;
+            margin-top: 2px;
+
+            * {
+                stroke: $secondary-color
+            }
         }
     }
 </style>
