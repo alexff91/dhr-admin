3fc72eb8fb6486c77b1a32e8613998a0764fb27f
lesha <oboshto@gmail.com>
2018-06-12 22:05:35 +0300

    vacancies table improvments

--- src/views/pages/vacancies.vue
+++ src/views/pages/vacancies.vue
@@ -7,21 +7,22 @@
         <div class="imba-table">
             <div class="imba-row imba-row-head">
                 <div class="imba-col imba-col-main">Позиция</div>
-                <div class="imba-col">Статус</div>
-                <div class="imba-col">Отклики</div>
+                <div class="imba-col imba-col-small">Статус</div>
+                <div class="imba-col imba-col-small">Отклики</div>
                 <div class="imba-col imba-col-small">Создана</div>
             </div>
             <router-link :to="`/vacancies/${vacancy.id}`" v-for="vacancy in vacancies" :key="vacancy.id" class="vacancy-row imba-row">
+                <div class="vacancy-status-block is-active"></div>
                 <div class="imba-col imba-col-main">
                     {{vacancy.position}}
                 </div>
-                <div class="imba-col">
+                <div class="imba-col imba-col-small">
                     В работе
                 </div>
-                <div class="imba-col">
-                    11/65
+                <div class="imba-col imba-col-small" title="Откликов/Просмотров">
+                    <b>11</b>/65
                 </div>
-                <div class="vacancy-creation-time imba-col imba-col-small">
+                <div class="vacancy-creation-time imba-col imba-col-small" :title="new Date(vacancy.creationDate).toLocaleString()">
                     <vk-icon icon="clock" class="icon" :ratio="0.7"></vk-icon>
                     <span>{{ distanceInWords(new Date(vacancy.creationDate), new Date(), { locale: ru }) }} назад</span>
                 </div>
@@ -76,15 +77,18 @@
     @import "../../assets/styles/variables";
 
     .imba-row {
+        position: relative;
         display: flex;
         flex-direction: row;
         align-items: center;
-        margin-bottom: .5rem;
 
-        padding: 0 1rem;
+        .imba-col.imba-col-main {
+            flex-basis: 250px;
+            flex-grow: 4;
+        }
 
         .imba-col {
-            flex-grow: 3;
+            flex-grow: 2;
             flex-shrink: 0;
             flex-basis: 75px;
         }
@@ -92,31 +96,44 @@
         .imba-col-small {
             flex-grow: 1;
         }
-
-        .imba-col-main {
-            flex-basis: 250px;
-            flex-grow: 0;
-        }
     }
 
     .vacancy-row {
         border-radius: 3px;
         background-color: #fff;
-        padding: 1rem;
-        color: $base-color;
+        padding: 0 0.5rem 0 1rem;
+        color: #211A1E;
         text-decoration: none;
-        height: 60px;
+        height: 45px;
+        font-size: 15px;
+        margin-bottom: 5px;
 
         &:hover {
-            box-shadow: 0 0 0 2px $primary-color;
+            box-shadow: 0 0 0 2px #e4e4e4;
+            background-color: #fefeff;
+        }
+
+        &:active {
+            box-shadow: 0 0 0 1px #e4e4e4;
         }
     }
 
     .imba-row-head {
         background-color: transparent;
-        font-size: 14px;
+        font-size: 13px;
         font-weight: bold;
-        color: grey
+        color: grey;
+        padding: 0 0.5rem 0 1rem;
+        margin-bottom: 4px;
+    }
+
+    .vacancy-status-block {
+        width: 6px;
+        height: 100%;
+        position: absolute;
+        left: 0;
+        background-color: #24bb64;
+        border-radius: 3px 0 0 3px;
     }
 
     .vacancy-creation-time {
