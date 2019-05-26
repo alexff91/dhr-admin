524bda4ba83b361505e42faf8cac5638557fd089
kirill belokon <kbelokon@textback.io>
2019-05-26 14:18:38 +0300

    change backend_URL

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = 'http://dhr';
+const BACKEND_URL = 'https://backend.dhr.cloud';
 
 const instance = axios.create({
   baseURL: `${BACKEND_URL}/api/v1`,
